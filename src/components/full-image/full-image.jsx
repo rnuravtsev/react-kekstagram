212f3ca999fa925223f412d761826275828625b3
rnuravtsev-macbook <muravtsev21@gmail.com>
2021-06-21 21:19:37 +0300

    Добавил возможность закрытия модального окна кликом вне модального окна

--- src/components/full-image/full-image.jsx
+++ src/components/full-image/full-image.jsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import isEmpty from "lodash/isEmpty"
 import { useDispatch, useSelector } from "react-redux"
+import OutsideClickHandler from "react-outside-click-handler/esm/OutsideClickHandler"
 
 import { closeFullImage } from "../../store/slices"
 import CommentsList from "../comments-list/comments-list"
@@ -19,61 +20,65 @@ const FullImage = () => {
     <>
       {!isEmpty(currentFullImage) && (
         <section className="big-picture overlay">
-          <h2 className="big-picture__title  visually-hidden">
+          <h2 className="big-picture__title visually-hidden">
             Просмотр фотографии
           </h2>
-          <div className="big-picture__preview">
-            <div className="big-picture__img">
-              <img src={url} alt={description} width="600" height="600" />
-            </div>
-
-            <div className="big-picture__social  social">
-              <div className="social__header">
-                <img
-                  className="social__picture"
-                  src="img/avatar-1.svg"
-                  alt="Аватар автора фотографии"
-                  width="35"
-                  height="35"
-                />
-                <p className="social__caption">{description}</p>
-                <p className="social__likes">
-                  Нравится <span className="likes-count">{likes}</span>
-                </p>
+          <OutsideClickHandler
+            onOutsideClick={() => dispatch(closeFullImage())}
+          >
+            <div className="big-picture__preview">
+              <div className="big-picture__img">
+                <img src={url} alt={description} width="600" height="600" />
               </div>
-              <CommentsList comments={comments} />
-              <div className="social__footer">
-                <img
-                  className="social__picture"
-                  src="img/avatar-6.svg"
-                  alt="Аватар комментатора фотографии"
-                  width="35"
-                  height="35"
-                />
-                <input
-                  type="text"
-                  className="social__footer-text"
-                  placeholder="Ваш комментарий..."
-                />
-                <button
-                  type="button"
-                  className="social__footer-btn"
-                  name="button"
-                >
-                  Отправить
-                </button>
-              </div>
-            </div>
 
-            <button
-              type="button"
-              onClick={onButtonClose}
-              className="big-picture__cancel  cancel"
-              id="picture-cancel"
-            >
-              Закрыть
-            </button>
-          </div>
+              <div className="big-picture__social  social">
+                <div className="social__header">
+                  <img
+                    className="social__picture"
+                    src="img/avatar-1.svg"
+                    alt="Аватар автора фотографии"
+                    width="35"
+                    height="35"
+                  />
+                  <p className="social__caption">{description}</p>
+                  <p className="social__likes">
+                    Нравится <span className="likes-count">{likes}</span>
+                  </p>
+                </div>
+                <CommentsList comments={comments} />
+                <div className="social__footer">
+                  <img
+                    className="social__picture"
+                    src="img/avatar-6.svg"
+                    alt="Аватар комментатора фотографии"
+                    width="35"
+                    height="35"
+                  />
+                  <input
+                    type="text"
+                    className="social__footer-text"
+                    placeholder="Ваш комментарий..."
+                  />
+                  <button
+                    type="button"
+                    className="social__footer-btn"
+                    name="button"
+                  >
+                    Отправить
+                  </button>
+                </div>
+              </div>
+
+              <button
+                type="button"
+                onClick={onButtonClose}
+                className="big-picture__cancel  cancel"
+                id="picture-cancel"
+              >
+                Закрыть
+              </button>
+            </div>
+          </OutsideClickHandler>
         </section>
       )}
     </>
